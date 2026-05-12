f8a4a463a22ffbb3a0a08b7cc4c18c4ee60e005e
SonMyeongJin <smj2802@naver.com>
2026-05-12 18:10:21 +0900

    Refactor : avoid using useEffect by combining

--- src/components/pokemon/image.tsx
+++ src/components/pokemon/image.tsx
@@ -1,24 +1,47 @@
 'use client';
 
-import type { FC } from 'react';
+import { buttonStyle } from '@molecules/bananaButton/index.css';
+import { type FC, useMemo, useState } from 'react';
 
 export const Profile: FC = (isPika) => {
-  const pikaUrl =
-    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png';
+  // const [pikaUrl, setURL] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png");
+
   const hitokageUrl =
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png';
+
+  const [megaSinka, setMega] = useState(false);
+
+  const pikaUrl = useMemo(() => {
+    if (megaSinka) {
+      return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png';
+    }
+    return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png';
+  }, [megaSinka]);
 
   if (isPika) {
     return (
       <div>
-        <img alt="Hitokage" src={hitokageUrl} />
+        <img alt="Hitokage" src={pikaUrl} />
+        <button
+          className={buttonStyle}
+          onClick={() => setMega((megaSinka) => !megaSinka)}
+          type="button"
+        >
+          メガシンカ
+        </button>
       </div>
     );
   }
-
   return (
     <div>
-      <img alt="Pikachu" src={pikaUrl} />
+      <img alt="Pikachu" src={hitokageUrl} />
+      <button
+        className={buttonStyle}
+        onClick={() => setMega((megaSinka) => !megaSinka)}
+        type="button"
+      >
+        メガシンカ
+      </button>
     </div>
   );
 };
