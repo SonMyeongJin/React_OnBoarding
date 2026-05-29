7d13d87173d8a6fda3aa185cdab10556499feae2
SonMyeongJin <smj2802@naver.com>
2026-05-29 10:22:11 +0900

    Format : biome に自動改行を120文字に増やす

--- src/components/pokemon/organisms/PokeCard/index.tsx
+++ src/components/pokemon/organisms/PokeCard/index.tsx
@@ -1,45 +1,40 @@
-import type { ComponentProps, FC } from 'react';
+import type {ComponentProps, FC} from 'react';
 import Image from '../../atoms/image';
 import MegaButton from '../../atoms/megaButton';
 import Status from '../../atoms/status';
-import type { AnimationType } from '../../entities/pokemon/model/type';
+import type {AnimationType} from '../../entities/pokemon/model/type';
 import Skills from '../../molecules/skills';
-import {
-  fireAnimation,
-  pokeCardStyle,
-  skillsStyle,
-  tailAnimation,
-} from './index.css';
+import {fireAnimation, pokeCardStyle, skillsStyle, tailAnimation,} from './index.css';
 
 type PokeCardProps = ComponentProps<typeof Image> &
-  ComponentProps<typeof Status> &
-  ComponentProps<typeof Skills> & {
+    ComponentProps<typeof Status> &
+    ComponentProps<typeof Skills> & {
     onClickMegaSinka: () => void;
     isSkillsActive: boolean;
     Animation: AnimationType;
-  };
+};
 
 const PokeCard: FC<PokeCardProps> = (props: PokeCardProps) => {
-  return (
-    <div className={pokeCardStyle}>
-      {props.Animation === 'fire' && <div className={fireAnimation} />}
-      {props.Animation === 'tail' && <div className={tailAnimation} />}
+    return (
+        <div className={pokeCardStyle}>
+            {props.Animation === 'fire' && <div className={fireAnimation}/>}
+            {props.Animation === 'tail' && <div className={tailAnimation}/>}
 
-      <Image
-        imageUrl={props.imageUrl}
-        isDead={props.isDead}
-        pokeName={props.pokeName}
-      />
+            <Image
+                imageUrl={props.imageUrl}
+                isDead={props.isDead}
+                pokeName={props.pokeName}
+            />
 
-      <Status {...props} />
+            <Status {...props} />
 
-      <div className={skillsStyle}>
-        {props.isSkillsActive && <Skills {...props} />}
-      </div>
+            <div className={skillsStyle}>
+                {props.isSkillsActive && <Skills {...props} />}
+            </div>
 
-      <MegaButton onClick={props.onClickMegaSinka} />
-    </div>
-  );
+            <MegaButton onClick={props.onClickMegaSinka}/>
+        </div>
+    );
 };
 
 export default PokeCard;
