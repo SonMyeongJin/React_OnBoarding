be153911044cd0b70d8ba13374e82c67b3ca44be
SonMyeongJin <smj2802@naver.com>
2026-06-09 11:12:47 +0900

    feat : react form 勉強一旦止まる

--- src/moreconCopy/Organisms/product_Info/index.tsx
+++ src/moreconCopy/Organisms/product_Info/index.tsx
@@ -1,8 +1,47 @@
+'use client';
+
+import type {FormEvent} from 'react';
+
 function Product_Info() {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    fetch('/api/products', { body: formData, method: 'POST' });
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
   return (
-    <div className="product_info">
-      <h1>Product Info</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label style={{ display: 'block' }}>
+        Product Name
+        <input name="query" />
+      </label>
+      <hr />
+      <label style={{ display: 'block' }}>
+        Description Product
+        <button type="submit">버튼입니당</button>
+      </label>
+      <hr />
+      Product Image
+      <br />
+      <label>
+        option1
+        <input type="checkbox" />
+      </label>
+      <br />
+      <label>
+        option2
+        <input type="checkbox" />
+      </label>
+      <br />
+      <label>
+        option3
+        <input type="checkbox" />
+      </label>
+    </form>
   );
 }
 
